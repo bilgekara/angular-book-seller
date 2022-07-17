914168ea0d8d8b92f06e8477836fbb67e4ea0d26
bilgekara <karabilgenur@gmail.com>
2022-07-17 04:13:10 +0300

    register and login form with template driven form

--- src/app/guest/login/login.component.ts
+++ src/app/guest/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from "../../models/user.model";
+import {faUserCircle} from "@fortawesome/free-solid-svg-icons";
+import {AuthenticationService} from "../../services/authentication.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  user: User = new User();
+  faUser = faUserCircle;
+  errorMessage: string= "";
+  constructor( private authenticationService: AuthenticationService, private router: Router) { }
 
   ngOnInit(): void {
+    // aktif bir oturum kullanıcısı varsa kullanıcıyı profil sayfasına yönlendiricez
+    if(this.authenticationService.currentUserValue?.id){
+      this.router.navigate(['/profile']);
+      return;
+    }
+  }
+
+  login(){
+    this.authenticationService.register(this.user).subscribe(data =>{
+      this.router.navigate(['/login']);
+    }, err =>{
+      this.errorMessage = 'Username or password incorrect.';
+    })
   }
 
 }
